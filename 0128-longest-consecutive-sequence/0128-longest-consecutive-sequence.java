bd51cd3f787f5d0028d71b0e259917a3ff2bb5b8
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-07-17 14:08:50 +0530

    Time: 65 ms (5.11%), Space: 91 MB (77.7%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,63 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) return 0;
-        int maxCount = 1, count = 1;
-        Set<Integer> set = new HashSet<>();
 
-        for (int num : nums) {
-            set.add(num);
+        // MY LOGIC ---
+
+        // if (nums.length == 0) return 0;
+        // int maxCount = 1, count = 1;
+        // Set<Integer> set = new HashSet<>();
+
+        // for (int num : nums) {
+        //     set.add(num);
+        // }
+        // Integer []arr = set.toArray(new Integer[0]);
+        // Arrays.sort(arr);
+        // for (int i=0; i<arr.length-1; i++){
+        //     if(arr[i]+1 == arr[i+1]){
+        //         count++;
+        //     }
+        //     else{
+        //         count = 1;
+        //     }
+        //     if(maxCount<count){
+        //         maxCount = count;
+        //     }
+        // }
+        // return maxCount;
+
+        // OPTIMIZED ---
+
+        // if(nums.length == 0) return 0;
+
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for (int num : nums){
+            map.put(num, false);
         }
-        Integer []arr = set.toArray(new Integer[0]);
-        Arrays.sort(arr);
-        for (int i=0; i<arr.length-1; i++){
-            if(arr[i]+1 == arr[i+1]){
+        
+        int maxCount = 0;
+
+        for(int num : nums){
+            int count = 1;
+
+            map.put(num, true);
+
+            int nextNum = num+1;
+            while(map.containsKey(nextNum) && map.get(nextNum) == false){
+                map.put(nextNum, true);
                 count++;
+                nextNum++;
             }
-            else{
-                count = 1;
+            
+            int  prevNum = num-1;
+            while(map.containsKey(prevNum) && map.get(prevNum) == false){
+                map.put(prevNum, true);
+                count++;
+                prevNum--;
             }
-            if(maxCount<count){
-                maxCount = count;
-            }
+
+            maxCount = Math.max(count, maxCount);
         }
+
         return maxCount;
     }
 }
