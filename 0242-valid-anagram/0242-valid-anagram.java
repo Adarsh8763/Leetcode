1a13adbd185090af1debb14bca7f9e70c1e8f643
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-07-21 12:49:18 +0530

    Time: 3 ms (93.43%), Space: 44 MB (97.97%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int sLen = s.length();
+        int tLen = t.length();
+
+        if(sLen != tLen) return false;
+
+        int [] arr = new int[60];
+        for (int i=0; i<sLen; i++){
+            char ch = s.charAt(i);
+            arr[ch-65]++;
+        }
+
+        for(int i=0; i<tLen; i++){
+            char ch = t.charAt(i);
+            arr[ch-65]--;
+        }
+
+        for (int i=0; i<arr.length; i++){
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
