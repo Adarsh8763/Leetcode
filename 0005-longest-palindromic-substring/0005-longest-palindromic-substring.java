7951990dd09af237447fcc96744e9a45e6301303
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-07-22 16:33:47 +0530

    Time: 14 ms (90.16%), Space: 43.8 MB (46.07%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,60 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String res = "";
 
-        for(int i=0; i<s.length(); i++){
-            for(int j=i; j<s.length(); j++){
-                if(isPalindrome(s, i, j) == true){
-                    if(res.length() < (j-i+1)){
-                        res = s.substring(i, j+1);
-                    }
-                }
+        // =========
+        // Brute Force
+        // =========
+
+        // String res = "";
+
+        // for(int i=0; i<s.length(); i++){
+        //     for(int j=i; j<s.length(); j++){
+        //         if(isPalindrome(s, i, j) == true){
+        //             if(res.length() < (j-i+1)){
+        //                 res = s.substring(i, j+1);
+        //             }
+        //         }
+        //     }
+        // }
+        // return res;
+
+
+        // =========
+        // Optimized one
+        // =========
+
+        int sLen = s.length();
+        int start = 0, end = 0;
+
+        for(int i=0; i<sLen; i++){
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i+1);
+            int len = Math.max(len1, len2);
+
+            if(len > (end-start+1)){
+                start = i-(len-1)/2;
+                end = i+len/2;
             }
         }
-        return res;
+        return s.substring(start, end+1);
     }
 
-    public boolean isPalindrome(String s, int i, int j){
-        while(i<j){
-            if(s.charAt(i) == s.charAt(j)){
-                i++;
-                j--;
-            }
-            else return false;
+    public int expand(String s, int left, int right){
+        while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
         }
-        return true;
+        return right-left-1;
     }
+
+    // public boolean isPalindrome(String s, int i, int j){
+    //     while(i<j){
+    //         if(s.charAt(i) == s.charAt(j)){
+    //             i++;
+    //             j--;
+    //         }
+    //         else return false;
+    //     }
+    //     return true;
+    // }
 }
