1bbe004720630ab16c8b1be90c250ceebd941f03
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-08-27 20:30:53 +0530

    Time: 0 ms (100%), Space: 110.9 MB (43.17%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,18 +1,25 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int len = edges.length;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int len = edges.length;
 
-        for(int[] edge : edges){
-            int u = edge[0];
-            int v = edge[1];
+        // for(int[] edge : edges){
+        //     int u = edge[0];
+        //     int v = edge[1];
 
-            map.put(u, map.getOrDefault(u, 0)+1);
-            map.put(v, map.getOrDefault(v, 0)+1);
+        //     map.put(u, map.getOrDefault(u, 0)+1);
+        //     map.put(v, map.getOrDefault(v, 0)+1);
 
-            if(map.get(u) == len) return u;
-            if(map.get(v) == len) return v;
-        }
-        return -1;
+        //     if(map.get(u) == len) return u;
+        //     if(map.get(v) == len) return v;
+        // }
+        // return -1;
+
+        int[] pair1 = edges[0]; // u1,v1
+        int[] pair2 = edges[1]; // (u1,v2 or v2,u1) ya (u2,v1 or v1,u2)
+
+        if(pair1[0] == pair2[0] || pair1[0] == pair2[1]) return pair1[0];
+        
+        return pair1[1];
     }
 }
