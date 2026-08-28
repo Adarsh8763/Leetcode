14c8cc3d4c36970c7a2f750cc26e1be67784176c
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-08-28 19:46:26 +0530

    Time: 0 ms (100%), Space: 47.1 MB (11.06%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,17 +4,17 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         rows = image.length;
         cols = image[0].length;
-        boolean[][] visited = new boolean[rows][cols];
+        // boolean[][] visited = new boolean[rows][cols];
 
-        dfs(sr, sc, image, visited, color, image[sr][sc]);
+        dfs(sr, sc, image, color, image[sr][sc]);
 
         return image;
     }
-    private void dfs(int row, int col, int[][] image, boolean[][] visited, int newColor, int currColor){
-        if(row < 0 || row >= rows || col < 0 || col >= cols || visited[row][col] || image[row][col] != currColor){
+    private void dfs(int row, int col, int[][] image, int newColor, int currColor){
+        if(row < 0 || row >= rows || col < 0 || col >= cols || image[row][col] == newColor || image[row][col] != currColor){
             return;
         }
-        visited[row][col] = true;
+        // visited[row][col] = true;
         image[row][col] = newColor;
 
         int[][] adj = {{row-1, col}, {row, col+1}, {row+1, col}, {row, col-1}};
@@ -22,7 +22,7 @@ class Solution {
         // if after increasing or decreasing row/col if idx out of bound hua tb dfs call hoga and dfs starting condition mei hi stop ho jyega
         for(int[] neighbour : adj){
             //Jo starting node ka color h wo chalta aayega aur aise hi dfs mei bhejata rahega
-            dfs(neighbour[0], neighbour[1], image, visited, newColor, currColor);
+            dfs(neighbour[0], neighbour[1], image, newColor, currColor);
         }
     }
 }
