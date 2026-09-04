e465e5aed484f526611ff38239c128cbfd19ef5f
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-09-04 08:30:12 +0530

    Time: 0 ms (100%), Space: 42.1 MB (33.41%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -7,18 +7,35 @@ class Solution {
     // }
 
 
-    //===== Memoization =======
+    //===== Memoization with SC=O(n) =======
+    // public int fib(int n) {
+    //     if(n <= 1) return n;
+    //     int[] memoArr = new int[n+1];
+
+    //     memoArr[0] = 0;
+    //     memoArr[1] = 1;
+
+    //     for(int i=2; i<=n; i++){
+    //         memoArr[i] = memoArr[i-2] + memoArr[i-1];
+    //     } 
+
+    //     return memoArr[n];
+    // }
+
+    //===== Memoization with SC=O(1) =======
     public int fib(int n) {
         if(n <= 1) return n;
-        int[] memoArr = new int[n+1];
 
-        memoArr[0] = 0;
-        memoArr[1] = 1;
+        int num1 = 0;
+        int num2 = 1;
+        int res = 1;
 
         for(int i=2; i<=n; i++){
-            memoArr[i] = memoArr[i-2] + memoArr[i-1];
+            res = num1 + num2;
+            num1 = num2;
+            num2 = res;
         } 
 
-        return memoArr[n];
+        return res;
     }
 }
