c5d6f6e6de0970375ec8a79b3b78428e0b01db37
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-09-06 15:29:26 +0530

    Time: 0 ms (100%), Space: 43.8 MB (47.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,33 +1,50 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        List<Integer> list = new ArrayList<>();
+        // List<Integer> list = new ArrayList<>();
 
-        int i=0;
-        int j=0;
+        // int i=0;
+        // int j=0;
 
-        while(i < m && j < n){
-            if(nums1[i] <= nums2[j]){
-                list.add(nums1[i]);
-                i++;
+        // while(i < m && j < n){
+        //     if(nums1[i] <= nums2[j]){
+        //         list.add(nums1[i]);
+        //         i++;
+        //     }
+        //     else{
+        //         list.add(nums2[j]);
+        //         j++;
+        //     }
+        // }
+        // while(i < m){
+        //     list.add(nums1[i]);
+        //     i++;
+        // }
+        // while(j < n){
+        //     list.add(nums2[j]);
+        //     j++;
+        // }
+
+        // int k = 0;
+        // for(int val : list){
+        //     nums1[k] = val;
+        //     k++;
+        // }
+
+        //===== Optimal ======
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while(i >= 0 && j >= 0){
+            if(nums1[i] >= nums2[j]){
+                nums1[k--] = nums1[i--];
             }
             else{
-                list.add(nums2[j]);
-                j++;
+                nums1[k--] = nums2[j--];
             }
         }
-        while(i < m){
-            list.add(nums1[i]);
-            i++;
-        }
-        while(j < n){
-            list.add(nums2[j]);
-            j++;
-        }
-
-        int k = 0;
-        for(int val : list){
-            nums1[k] = val;
-            k++;
-        }
+        while(j >= 0){
+            nums1[k--] = nums2[j--];
+        }        
     }
 }
