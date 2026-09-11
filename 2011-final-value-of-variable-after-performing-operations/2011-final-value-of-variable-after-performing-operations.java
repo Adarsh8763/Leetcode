c0c92ee7a4e6f4df7f35e5210c3fc5ea2a49593a
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-09-11 17:28:54 +0530

    Time: 0 ms (100%), Space: 44.6 MB (13.85%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -1,8 +1,22 @@
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
+
+        // ====== My approach ======
+        // int x = 0;
+        // for(String str : operations){
+        //     if(str.equals("--X") || str.equals("X--")){
+        //         x--;
+        //     }
+        //     else{
+        //         x++;
+        //     }
+        // }
+        // return x;
+
+        // ====== Most Optimal =====
         int x = 0;
         for(String str : operations){
-            if(str.equals("--X") || str.equals("X--")){
+            if(str.charAt(1) == '-'){
                 x--;
             }
             else{
