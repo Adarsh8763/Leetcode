263d76e026f916546db282e26cdb6af29d7f75c4
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-09-15 20:25:35 +0530

    Time: 3 ms (79.53%), Space: 43.8 MB (15.04%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,28 +1,56 @@
 class Solution {
     public int calPoints(String[] operations) {
-        List<Integer> record = new ArrayList<>();
+        // List<Integer> record = new ArrayList<>();
         
+        // for(String operation : operations){
+        //     int idx = record.size()-1;
+        //     if(operation.equals("C")){
+        //         record.remove(record.get(idx));
+        //     }
+        //     else if(operation.equals("D")){
+        //         record.add(record.get(idx)*2);
+        //     }
+        //     else if(operation.equals("+")){
+        //         int num1 = record.get(idx-1);
+        //         int num2 = record.get(idx);
+        //         record.add(num1+num2);
+        //     }
+        //     else{
+        //         record.add(Integer.parseInt(operation));
+        //     }
+        // }
+
+        // int score = 0;
+        // for(int num : record){
+        //     score += num;
+        // }
+
+        // return score;
+
+        Stack<Integer> record = new Stack<>();
+
         for(String operation : operations){
-            int idx = record.size()-1;
             if(operation.equals("C")){
-                record.remove(record.get(idx));
+                record.pop();
             }
             else if(operation.equals("D")){
-                record.add(record.get(idx)*2);
+                int prev = record.peek();
+                record.push(2*prev);
             }
             else if(operation.equals("+")){
-                int num1 = record.get(idx-1);
-                int num2 = record.get(idx);
-                record.add(num1+num2);
+                int num1 = record.pop();
+                int num2 = record.peek();
+                record.push(num1);
+                record.push(num1+num2);
             }
             else{
-                record.add(Integer.parseInt(operation));
+                record.push(Integer.parseInt(operation));
             }
         }
 
         int score = 0;
-        for(int num : record){
-            score += num;
+        while(!record.isEmpty()){
+            score += record.pop();
         }
 
         return score;
