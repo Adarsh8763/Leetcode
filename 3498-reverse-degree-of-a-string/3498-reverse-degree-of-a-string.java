d15e6a79ae95df71baa057fd63f44f494508e122
Adarsh Kumar Kamal <72192015+Adarsh8763@users.noreply.github.com>
2026-09-20 11:35:32 +0530

    Time: 1 ms (100%), Space: 44 MB (50.47%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,16 +1,30 @@
 class Solution {
     public int reverseDegree(String s) {
-        int[] freqArr = new int[26];
 
-        for(int i=0; i<26; i++){
-            freqArr[i] = 26-i;
-        }
+        // ==== My code =====
+        // int[] freqArr = new int[26];
+
+        // for(int i=0; i<26; i++){
+        //     freqArr[i] = 26-i;
+        // }
+
+        // int sum = 0;
+        // for(int i=0; i<s.length(); i++){
+        //     sum += freqArr[s.charAt(i)-97]*(i+1);
+        // }
+
+        // return sum;
+
+
+        // ===== Optimal code =====
+
 
         int sum = 0;
         for(int i=0; i<s.length(); i++){
-            sum += freqArr[s.charAt(i)-97]*(i+1);
+            int val = 26-(s.charAt(i)-97);
+            sum += val*(i+1);
         }
-
         return sum;
     }
+
 }
